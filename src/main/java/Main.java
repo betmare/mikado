f4354738bdae12af78d17fbad4d3760984a421f7
Betmart Aguilar <betmart0901@gmail.com>
2021-05-30 19:13:54 -0600

    Commit inicial, flujo principal registro de usuario para realizar compra.

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    private static String name;
+    private static String lastname;
+    private static String phone;
+    private static String address;
+    private static String email;
+
+    public static void main(String[] args) {
+        System.out.println("Registro de Usuario");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1. Ingrese su nombre: ");
+        name = scan.nextLine();
+        System.out.println("2. Ingrese su Apellido: ");
+        lastname = scan.nextLine();
+        System.out.println("3. Ingrese su telefono: ");
+        phone = scan.nextLine();
+        System.out.println("4. Ingrese su dirección ");
+        address = scan.nextLine();
+        System.out.println("5. Ingrese su email: ");
+        email = scan.nextLine();
+        System.out.println("Sus Datos son: Nombre: "+name+", Apellido: "+lastname+", telefono: "+phone+", Dirección: "+address+", correo electronico: "+email);
+        System.out.println("Son correctos? Y or n");
+        String answer = scan.nextLine();
+        if(answer.equals("Y")) {
+            System.out.println("Usuario Registrado Exitosamente: " + name + " " + lastname);
+        } else {
+            System.out.println("Usuario no Registrado");
+        }
+    }
+}
