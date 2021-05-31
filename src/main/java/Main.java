b76f708f784001d560cbe96c0490742fa38f62ee
Betmart Aguilar <betmart0901@gmail.com>
2021-05-30 19:35:43 -0600

    Agregar controlador para la logica de negocio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        System.out.println("Registro de Usuario");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("1. Ingrese su nombre: ");
-        customer.setName(scan.nextLine());
-        System.out.println("2. Ingrese su Apellido: ");
-        customer.setLastname(scan.nextLine());
-        System.out.println("3. Ingrese su telefono: ");
-        customer.setPhone(scan.nextLine());
-        System.out.println("4. Ingrese su dirección ");
-        customer.setAddress(scan.nextLine());
-        System.out.println("5. Ingrese su email: ");
-        customer.setEmail(scan.nextLine());
-        System.out.println("Sus Datos son: Nombre: "+customer.getName()+", Apellido: "+customer.getLastname()+", telefono: "+
-                customer.getPhone()+", Dirección: "+customer.getAddress()+", correo electronico: "+customer.getEmail());
-        System.out.println("Son correctos? Y or n");
-        String answer = scan.nextLine();
-        if(answer.equals("Y")) {
-            System.out.println("Usuario Registrado Exitosamente: " + customer.getName() + " " + customer.getLastname());
-        } else {
-            System.out.println("Usuario no Registrado");
-        }
+        RegisterController registerController = new RegisterController();
+        System.out.println(registerController.customerRegister());
     }
 }
