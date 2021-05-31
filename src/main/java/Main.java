58425187bcfcbd96c3dcac7a0ccefa3b2082d6d3
Betmart Aguilar <betmart0901@gmail.com>
2021-05-30 19:40:14 -0600

    Agregar Multiples clientes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,24 +6,31 @@ public class Main {
         Customer customer = new Customer();
         System.out.println("Registro de Usuario");
         Scanner scan = new Scanner(System.in);
-        System.out.println("1. Ingrese su nombre: ");
-        customer.setName(scan.nextLine());
-        System.out.println("2. Ingrese su Apellido: ");
-        customer.setLastname(scan.nextLine());
-        System.out.println("3. Ingrese su telefono: ");
-        customer.setPhone(scan.nextLine());
-        System.out.println("4. Ingrese su dirección ");
-        customer.setAddress(scan.nextLine());
-        System.out.println("5. Ingrese su email: ");
-        customer.setEmail(scan.nextLine());
-        System.out.println("Sus Datos son: Nombre: "+customer.getName()+", Apellido: "+customer.getLastname()+", telefono: "+
-                customer.getPhone()+", Dirección: "+customer.getAddress()+", correo electronico: "+customer.getEmail());
-        System.out.println("Son correctos? Y or n");
-        String answer = scan.nextLine();
-        if(answer.equals("Y")) {
-            System.out.println("Usuario Registrado Exitosamente: " + customer.getName() + " " + customer.getLastname());
-        } else {
-            System.out.println("Usuario no Registrado");
+        boolean nextCustomer = Boolean.TRUE;
+        // No es necesario agregar multiples clientes ya que el caso de uso es registro de un usuario asociado a un carrito
+        // de compra
+        while (nextCustomer) {
+            System.out.println("1. Ingrese su nombre: ");
+            customer.setName(scan.nextLine());
+            System.out.println("2. Ingrese su Apellido: ");
+            customer.setLastname(scan.nextLine());
+            System.out.println("3. Ingrese su telefono: ");
+            customer.setPhone(scan.nextLine());
+            System.out.println("4. Ingrese su dirección ");
+            customer.setAddress(scan.nextLine());
+            System.out.println("5. Ingrese su email: ");
+            customer.setEmail(scan.nextLine());
+            System.out.println("Sus Datos son: Nombre: "+customer.getName()+", Apellido: "+customer.getLastname()+", telefono: "+
+                    customer.getPhone()+", Dirección: "+customer.getAddress()+", correo electronico: "+customer.getEmail());
+            System.out.println("Son correctos? Y or n");
+            String answer = scan.nextLine();
+            if(answer.equals("Y")) {
+                System.out.println("Usuario Registrado Exitosamente: " + customer.getName() + " " + customer.getLastname());
+            } else {
+                System.out.println("Usuario no Registrado");
+            }
+            System.out.println("Desea Agregar otro cliente? Y/n");
+            nextCustomer = scan.nextLine().equals("Y");
         }
     }
 }
